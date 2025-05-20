abfe7077fd3055d7c7bd3f85bae76f25c856ffff
Jatin Yadav <jatin27yadav@gmail.com>
2025-05-21 01:26:58 +0530

    [FEAT]: Added side scrolls

--- components/dialrotation.tsx
+++ components/dialrotation.tsx
@@ -2,100 +2,86 @@
 
 import { useEffect, useRef } from "react";
 import gsap from "gsap";
-import { Draggable } from "gsap/Draggable";
-import { InertiaPlugin } from "gsap/InertiaPlugin";
-
-gsap.registerPlugin(Draggable, InertiaPlugin);
 
 const imageURLs = [
-  "https://uxwing.com/wp-content/themes/uxwing/download/transportation-automotive/automobile-icon.svg",
-  "https://uxwing.com/wp-content/themes/uxwing/download/internet-network-technology/hobbies-icon.svg",
-  "https://uxwing.com/wp-content/themes/uxwing/download/sport-and-awards/cricket-sport-icon.svg",
-  "https://uxwing.com/wp-content/themes/uxwing/download/sport-and-awards/ball-football-icon.svg",
+  "https://www.thiings.co/_next/image?url=https%3A%2F%2Flftz25oez4aqbxpq.public.blob.vercel-storage.com%2Fimage-BBXTV4LAEedO5jo0W6YkMkVyTlmflS.png&w=256&q=75",
+  "https://www.thiings.co/_next/image?url=https%3A%2F%2Flftz25oez4aqbxpq.public.blob.vercel-storage.com%2Fimage-lRbpg8oTjmaqBgWDr4lfslf9vaE6u9.png&w=256&q=75",
+  "https://www.thiings.co/_next/image?url=https%3A%2F%2Flftz25oez4aqbxpq.public.blob.vercel-storage.com%2Fimage-CX1AXPSpbznN7ptV0ESDhhWp3HJG9C.png&w=256&q=75",
+  "https://www.thiings.co/_next/image?url=https%3A%2F%2Flftz25oez4aqbxpq.public.blob.vercel-storage.com%2Fimage-umsNhCOvityhX7YwcgZ0YerJKZD3XO.png&w=256&q=75",
+  "https://www.thiings.co/_next/image?url=https%3A%2F%2Flftz25oez4aqbxpq.public.blob.vercel-storage.com%2Fimage-6Inz57rRx6BwWwDRsQO8nsSfyUVz1c.png&w=256&q=75",
+  "https://www.thiings.co/_next/image?url=https%3A%2F%2Flftz25oez4aqbxpq.public.blob.vercel-storage.com%2Fimage-IPe6fFeR1w6DVZ2143Y6YCLibSOdq7.png&w=256&q=75",
+  "https://www.thiings.co/_next/image?url=https%3A%2F%2Flftz25oez4aqbxpq.public.blob.vercel-storage.com%2Fimage-ob5X6P6AiLzXqRjJXCOedM4RaJ3Joa.png&w=256&q=75"
 ];
 
-const CircleSpinner = () => {
-  const circleRef = useRef<HTMLDivElement>(null);
-  const imagesRef = useRef<HTMLImageElement[]>([]);
+type Props = {
+  direction?: "up" | "down";
+  position?: "left" | "right";
+  offset?: number; // e.g., 40
+};
+
+const CircleSpinner = ({
+  direction = "up",
+  position = "left",
+  offset = 0,
+}: Props) => {
+  const trackRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    if (!circleRef.current) return;
+    if (!trackRef.current) return;
 
-    const circle = circleRef.current;
-    const radius = circle.offsetWidth / 2;
-    const angleIncrement = (Math.PI * 2) / imageURLs.length;
+    const fromY = direction === "up" ? 0 : "-50%";
+    const toY = direction === "up" ? "-50%" : "0%";
 
-    const images: HTMLImageElement[] = [];
-
-    imageURLs.forEach((url, i) => {
-      const img = new Image();
-      img.src = url;
-      img.style.position = "absolute";
-      img.style.top = "0";
-      img.style.left = "0";
-      img.style.transformOrigin = "50% 50%";
-      img.style.width = "70px";
-      img.style.height = "70px";
-
-      const angle = angleIncrement * i;
-      gsap.set(img, {
-        xPercent: -50,
-        yPercent: -50,
-        x: radius + Math.cos(angle) * radius,
-        y: radius + Math.sin(angle) * radius,
-      });
-
-      circle.appendChild(img);
-      images.push(img);
-    });
-
-    imagesRef.current = images;
-
-    const spin = gsap.timeline({
-      repeat: -1,
-      defaults: { duration: 50, ease: "none" },
-    });
-
-    spin.to(circle, { rotation: 360 }).to(images, { rotation: -360 }, 0);
-
-    const draggableInstance = Draggable.create(circle, {
-      type: "rotation",
-      inertia: true,
-      onThrowUpdate: function () {
-        const angle = (this.rotation + 360 * 100000) % 360;
-        spin.progress(angle / 360);
-      },
-      onThrowComplete: () => {
-        spin.resume();
-        gsap.fromTo(
-          spin,
-          { timeScale: 0 },
-          { duration: 1, timeScale: 1, ease: "power1.in" }
-        );
-      },
-    })[0];
+    const marquee = gsap.fromTo(
+      trackRef.current,
+      { y: fromY },
+      {
+        y: toY,
+        ease: "none",
+        duration: 80,
+        repeat: -1,
+      }
+    );
 
     return () => {
-      spin.kill();
-      draggableInstance.kill();
-      images.forEach((img) => img.remove());
+      marquee.kill();
     };
-  }, []);
+  }, [direction]);
+
+  const positionStyle =
+    position === "left" ? { left: `${offset}px` } : { right: `${offset}px` };
 
   return (
-    <>
-      <div
-        className="w-80 h-80 relative mx-auto mt-20 mr-130"
-        style={{ zIndex: 1, opacity: 0.7 }}
-      >
-        <div
-          ref={circleRef}
-          className="main-circle w-full h-full rounded-full relative"
-        ></div>
-
-        <style jsx>{``}</style>
+    <div
+      className="top-0 w-40 h-screen overflow-hidden z-10"
+      style={{
+        ...positionStyle,
+        WebkitMaskImage:
+          "linear-gradient(to bottom, transparent, black 20%, black 80%, transparent)",
+        maskImage:
+          "linear-gradient(to bottom, transparent, black 20%, black 80%, transparent)",
+        WebkitMaskRepeat: "no-repeat",
+        maskRepeat: "no-repeat",
+      }}
+    >
+      <div ref={trackRef} className="flex flex-col gap-12">
+        {[
+          ...imageURLs,
+          ...imageURLs,
+          ...imageURLs,
+          ...imageURLs,
+          ...imageURLs,
+          ...imageURLs,
+        ].map((url, idx) => (
+          <img
+            key={idx}
+            src={url}
+            alt={`icon-${idx}`}
+            className="w-30 h-30 object-contain mx-auto"
+          />
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
